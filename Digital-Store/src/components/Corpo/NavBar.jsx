d3083782c20f606e66ac5d888488fff8b49b8bb2
BrenoBar51 <brenobarcelos9@gmail.com>
2023-08-07 19:38:11 -0300

    NavBar

---
+++ Digital-Store/src/components/Corpo/NavBar.jsx
@@ -0,0 +1,21 @@
+import Buttons from "../Buttons";
+import Carrinho from "./Carrinho";
+import Logo from "./Logo";
+import PageButtons from "./PageButtons";
+import SearchBar from "./SearchBar";
+
+export default function NavBar(){
+    return(
+        <div className="bg-white justify-center">
+            <div className="flex space-x-44 m-8 justify-center mb-12">
+                <Logo />
+                <SearchBar />
+                <Buttons />
+                <Carrinho />
+            </div>
+            <div className="flex space-x-16 justify-start">
+                <PageButtons />
+            </div>
+        </div>
+    )
+}
