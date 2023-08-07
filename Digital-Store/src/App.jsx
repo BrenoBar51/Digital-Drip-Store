f508f58487574122fd9b75a78026e84d2f2f624b
BrenoBar51 <brenobarcelos9@gmail.com>
2023-08-07 09:19:42 -0300

    adicionando extensões

--- Digital-Store/src/App.jsx
+++ Digital-Store/src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css'
 import Header from './components/organism/Header'
 import Body from './components/organism/Body'
+import { RouterProvider } from 'react-router-dom'
+import { router } from "./routes"
 
 function App() {
   return (
     <>
+      <RouterProvider router={router} />
       <Header />
       <Body />
     </>
