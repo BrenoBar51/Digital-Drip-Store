865b021b7eba1c3503736b7d8dbdd751905f1710
BrenoBar51 <brenobarcelos9@gmail.com>
2023-08-07 19:52:49 -0300

    pastas bugadas resolvidas

--- Digital-Store/src/components/Corpo/Buttons.jsx
+++
@@ -1,8 +0,0 @@
-export default function Buttons(){
-    return(
-        <div className="flex space-x-9">
-            <button className="underline">Cadastre-se</button>
-            <button className="bg-[#C92071] text-white">Entrar</button>
-        </div>
-    )
-}
