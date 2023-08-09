4d8b3b9ca35020101b352be16c41ed5c3499a6f2
VicMaria23 <vick4122@gmail.com>
2023-08-09 08:26:47 -0300

    alteraçoes nas redes sociais

--- Digital-Store/src/components/AboutUs.jsx
+++ Digital-Store/src/components/AboutUs.jsx
@@ -5,7 +5,7 @@ export default function AboutUs(){
         <div className="m-6 grid grid-rows-3">
             <div><LogoSemCor /></div>
             <div className="text-white font-normal">Lorem ipsum dolor sit amet, consectetur<br/>adipiscing elit, sed do eiusmod tempor<br/>incididunt ut labore et dolore.</div>
-            <div className="pt-6 , px-6">
+            <div className="pt-6  flex gap-4">
             <box-icon name='facebook' type='logo' color='#ffffff' ></box-icon>
             <box-icon name='instagram' type='logo' color='#ffffff' ></box-icon> 
             <box-icon name='twitter' type='logo' color='#ffffff' ></box-icon>
