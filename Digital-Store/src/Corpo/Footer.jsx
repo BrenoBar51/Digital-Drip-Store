d9592f4a9a67ca8c0d391c8cf4c81216e33a2b67
VicMaria23 <vick4122@gmail.com>
2023-08-09 09:46:24 -0300

    bug

--- Digital-Store/src/Corpo/Footer.jsx
+++ Digital-Store/src/Corpo/Footer.jsx
@@ -1,18 +1,20 @@
 import AboutUs from "../components/AboutUs";
 import Categorias from "../components/Categorias";
 import Contato from "../components/Contato";
+import Copyright from "../components/Copyright";
 import Informacoes from "../components/Informacoes";
 
 export default function Footer(){
     return(
-        <div className="">
-            <footer className="bg-[#1F1F1F] flex">
-                <div className="grid grid-cols-4" >
+        <div>
+            <footer className="bg-[#1F1F1F] flex flex-col">
+                <div className="grid ml-[7%] mt-[4%] mr-[6%] grid-cols-4" >
                   <div className="col-span-1"> <AboutUs /> </div> 
                   <div className="row-span-3">   <Informacoes /> </div>
                   <div className="col-span-6s"> <Categorias /> </div>  
-                    <Contato />
+                  <div className="col-span-2s"> <Contato /></div> 
                 </div>
+                  <Copyright />
             </footer>
         </div>
     )
