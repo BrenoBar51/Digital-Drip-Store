29912d90c8afe4921d49d10852600b35d008599f
BrenoBar51 <brenobarcelos9@gmail.com>
2023-08-09 09:56:23 -0300

    fix

--- Digital-Store/src/components/Informacoes.jsx
+++ Digital-Store/src/components/Informacoes.jsx
@@ -4,14 +4,12 @@ export default function Informacoes(){
           <div className="text-white font-normal, ps-40">
             <h6 className="pt-14">Informaçoes</h6>
           <div className="pt-6">  
-            
                 <div className="cursor-pointer">Sobre o Drip</div>
                 <div className="cursor-pointer">Segurança</div>
                 <div className="cursor-pointer">Wishlist</div>
                 <div className="cursor-pointer">Blog</div>
                 <div className="cursor-pointer">Trabalhe conosco</div>
                 <div className="cursor-pointer">Meus pedidos</div>
-            
             </div>
           </div>
         </div>
