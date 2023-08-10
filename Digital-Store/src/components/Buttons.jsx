186f0240b7b072c7864dd05b53328731d8be8aa6
berlimjr <gameruser815@gmail.com>
2023-08-10 16:49:14 -0300

    ajeitando os botoes

--- Digital-Store/src/components/Buttons.jsx
+++ Digital-Store/src/components/Buttons.jsx
@@ -4,9 +4,9 @@ export default function Buttons(props){
             <button className="text-[#474747] underline">
                 {props.value}
             </button>
-            <button className={`w-[114px] h-[40px] flex-shrink-0 bg-[#C92071] text-[#f5f5f5] rounded-lg hover:bg-[#c92085] hover:transition-[2s]
-
-                ${props.type === 'retangulo' && 'w-[220px] h-[48px]'}
+            <button className={`w-[114px] h-[40px] flex-shrink-0 bg-[#C92071]  rounded-lg hover:bg-[#c92085] hover:transition-[2s]
+                ${props.value2 === 'Entrar' && 'text-[#ffffff]'}
+                ${props.type === 'retangulo' && 'w-[220px] h-[48px] text-[#ffffff] '}
                 ${props.edition === 'inverso' && 'bg-[#ffffff] text-[#c92071] z-20 w-[153px] h-[48px] absolute text-[16px] not-italic font-bold leading-[24px] tracking-[0.75px] text-center'}
             `}>
                 {props.value2}
