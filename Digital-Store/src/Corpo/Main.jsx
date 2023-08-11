63c3957f496dc66a5b7a006dd4740c50a0e44a51
berlimjr <gameruser815@gmail.com>
2023-08-11 15:58:05 -0300

    Criação dos 5 cards de blusa e etc, pt2 da section 2

--- Digital-Store/src/Corpo/Main.jsx
+++ Digital-Store/src/Corpo/Main.jsx
@@ -2,6 +2,7 @@ import Carrossel from "../components/Carrossel";
 import TitulosMain from "../components/TitulosMain";
 
 import CardThree from "../components/section2/CardsThree";
+import ColectionCards from "../components/section2/ColectionCards";
 
 import AirJordanPage from "./AirJordanPage";
  
@@ -9,15 +10,16 @@ import AirJordanPage from "./AirJordanPage";
 
 export default function Main() {
     return (
-        <main className="flex flex-col w-full h-full   p-4  box-border">
+        <main className="flex flex-col w-full h-full bg-[#F9F8FE]  p-4  box-border">
             <section className="flex w-full h-611 bg-[#f5f5f5]">
                 <TitulosMain />
                 <Carrossel />
             </section>
-            <section className="w-full h-[1000px]">
+            <section className="w-full  h-[1000px] bg-[#F9F8FE]">
                 <CardThree />
+                <ColectionCards />
             </section>
-
+            
         </main>
     )
 }
