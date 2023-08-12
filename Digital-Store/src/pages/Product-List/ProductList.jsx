f1c8aeac6c4a08a76aa4e47c1c2c18666ec42d31
Yan Felipe <yanfelipe9@hotmail.com>
2023-08-12 13:38:37 -0300

    adicionando a lista dos produtos na page Product-List, criei uma pasta de utils e ajustei o eslint

--- Digital-Store/src/pages/Product-List/ProductList.jsx
+++ Digital-Store/src/pages/Product-List/ProductList.jsx
@@ -1,13 +1,34 @@
-import { Card } from "../../components/Card"
-import NavBar from "../../components/NavBar"
-import SideBar from "../../components/SideBar"
+import Footer from "../../Corpo/Footer";
+import { Card } from "../../components/Card";
+import NavBar from "../../components/NavBar";
+import SideBar from "../../components/SideBar";
+import { cardsInfo } from "../../utils/mocks/cardsInfo";
 
 export default function ProductList() {
-    return (
-        <section>
-            <NavBar />
-            <SideBar />
-            <Card />
-        </section>
-    )
+  return (
+    <>
+      <NavBar />
+      <section className="flex mx-[70px] mb-28 mt-10 gap-1">
+        <aside className="w-[308px] h-[720px]">
+          <SideBar />
+        </aside>
+        <div className="flex flex-wrap w-[904px]">
+            {cardsInfo.map((card) => {
+                return(
+                    <Card 
+                    key={card.id} 
+                    discountPercentual={card.discountPercentual} 
+                    category={card.category} 
+                    productName={card.productName}
+                    discountPrice={card.discountPrice}
+                    price={card.price}
+                    isDiscount={card.isDiscount}
+                    url={card.url} />
+                )
+            })}
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
 }
