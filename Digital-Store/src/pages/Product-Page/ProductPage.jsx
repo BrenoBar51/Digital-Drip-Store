a90a77561a31807484115f208f76e20a3c94112b
arielgam <ari.gomes.mendes@gmail.com>
2023-08-14 09:37:25 -0300

    linkfigma

---
+++ Digital-Store/src/pages/Product-Page/ProductPage.jsx
@@ -0,0 +1,11 @@
+import Footer from "../../Corpo/Footer";
+import Header from "../../Corpo/Header";
+
+export default function ProductPage() {
+    return(
+        <div>
+            <Header />
+            <Footer />
+        </div>
+    )
+}
