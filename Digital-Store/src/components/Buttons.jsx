0777e34e69b3ec06c25bbe144541a7496e951c6d
WesBru31 <weslleybba@gmail.com>
2023-08-14 08:42:36 -0300

    correção dos hovers dos botoes

--- Digital-Store/src/components/Buttons.jsx
+++ Digital-Store/src/components/Buttons.jsx
@@ -7,7 +7,7 @@ export default function Buttons(props){
             <button className={`w-[114px] h-[40px] flex-shrink-0 bg-[#C92071]  rounded-lg hover:bg-[#c92085] hover:transition-[2s]
                 ${props.value2 === 'Entrar' && 'text-[#ffffff]'}
                 ${props.type === 'retangulo' && 'w-[220px] h-[48px] text-[#ffffff] '}
-                ${props.edition === 'inverso' && 'bg-[#ffffff] text-[#c92071] z-20 w-[153px] h-[48px] absolute text-[16px] not-italic font-bold leading-[24px] tracking-[0.75px] text-center'}
+                ${props.edition === 'inverso' && 'bg-[#ffffff] text-[#c92071] z-20 w-[153px] h-[48px] absolute text-[16px] not-italic font-bold leading-[24px] tracking-[0.75px] text-center hover:bg-indigo-300 hover:text-[#fff] hover:duration-500 hover:ease-in-out duration-[1.4s]'}
             `}>
                 {props.value2}
                 {props.value3}
