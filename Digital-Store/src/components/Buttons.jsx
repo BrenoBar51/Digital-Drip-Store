bd52b66bfa7cfd9b963ffb5932ac68a62f9963bb
WesBru31 <weslleybba@gmail.com>
2023-08-16 09:02:35 -0300

    hover dos botoes

--- Digital-Store/src/components/Buttons.jsx
+++ Digital-Store/src/components/Buttons.jsx
@@ -9,8 +9,8 @@ export default function Buttons(props){
                 </NavLink>
             </button>
             <button className={`
-                ${props.value2 === 'Entrar' && 'text-[#ffffff] w-[114px] h-[40px] flex-shrink-0 bg-[#C92071]  rounded-lg hover:bg-[#c92085] hover:transition-[2s]'}
-                ${props.type === 'retangulo' && 'w-[220px] h-[48px] text-[#ffffff] flex-shrink-0 bg-[#C92071]  rounded-lg hover:bg-[#c92085] hover:transition-[2s]'}
+                ${props.value2 === 'Entrar' && 'text-[#ffffff] w-[114px] h-[40px] flex-shrink-0 bg-[#C92071]  rounded-lg hover:bg-[#991956] hover:transition-[2s]'}
+                ${props.type === 'retangulo' && 'w-[220px] h-[48px] text-[#ffffff] flex-shrink-0 bg-[#C92071]  rounded-lg hover:bg-[#991956] hover:transition-[2s]'}
                 ${props.edition === 'inverso' && 'rounded-lg bg-[#ffffff] text-[#c92071] z-20 w-[153px] h-[48px] absolute text-[16px] not-italic font-bold leading-[24px] tracking-[0.75px] text-center hover:bg-indigo-300 hover:text-[#fff] hover:duration-500 hover:ease-in-out duration-[1.4s]'}
                 ${props.linkbtn === 'btnlink' && 'text-[#c92071] bg-[#f9f8fe]'}
             `}>
