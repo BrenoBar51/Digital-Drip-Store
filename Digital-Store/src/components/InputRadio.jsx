3b2f9a654d720b3cf5f5997269247f438630a66d
arielgam <ari.gomes.mendes@gmail.com>
2023-08-16 10:08:30 -0300

    alteraçoes nas redes sociais

---
+++ Digital-Store/src/components/InputRadio.jsx
@@ -0,0 +1,7 @@
+export default function InputRadio() {
+    return(
+        <div>
+            <input type="radio" name="" id="" class="w-4 h-4 text-[#C92071] bg-[#C92071] border-[#C92071] rounded focus:ring-[#f02788] dark:ring-offset-[#C92071] focus:ring-2 dark:bg-[#C92071] dark:border-[#C92071] accent-[#C92071]" />
+        </div>
+    )
+}
