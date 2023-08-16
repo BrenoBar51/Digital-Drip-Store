29169338b9ab142dbaede0e30bd88434ec93202b
WesBru31 <weslleybba@gmail.com>
2023-08-16 08:55:03 -0300

    atualizando a seta

--- Digital-Store/src/components/section3/TableCards.jsx
+++ Digital-Store/src/components/section3/TableCards.jsx
@@ -1,5 +1,6 @@
 import { cardsInfo } from "../../utils/mocks/cardsInfo";
 import { Card } from "../Card";
+import  Buttons from "../Buttons";
 import MicroTitle from "../section2/MIcroTitle";
 export default function TableCards() {
     const eightcard = cardsInfo.slice(0, 8);
@@ -10,8 +11,9 @@ export default function TableCards() {
     return (
         <>
             <div className="max-w-full h-[1000px] flex flex-col mx-[5.9%] gap-y-6">
-                    <div className="w-[1200px] h-[26px] px-[8px]">
+                    <div className="w-[1200px] h-[26px] px-[8px] flex justify-between">
                         <MicroTitle value3='Produtos em alta' h2s2pt2='h2s2' />
+                        <Buttons value3='Ver todos ->' linkbtn='btnlink'/>
                     </div>
                 <div className="w-[1200px] flex overflow-hidden  ">
                     <div className="flex flex-wrap">
