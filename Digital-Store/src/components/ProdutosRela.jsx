dea85031895fcdfd56ebf6a0ade4ae658f5b633b
SrOrfeu <orfeusancho@gmail.com>
2023-08-20 07:53:35 -0300

    pagina do produto finalizada

--- Digital-Store/src/components/ProdutosRela.jsx
+++
@@ -1,9 +0,0 @@
-export default function ProdutosRela() {
-    return(
-        <div>
-         <h1>Produtos Relacionados</h1>
-
-
-        </div>
-    )
-}
--- Digital-Store/src/pages/Product-Page/ProductPage.jsx
+++ Digital-Store/src/pages/Product-Page/ProductPage.jsx
@@ -1,21 +1,21 @@
 import Footer from "../../Corpo/Footer";
 import Header from "../../Corpo/Header";
-import ProductImage from "../../components/Product-image";
-import LocalPage from "../../components/ProductPageLocal";
-import ProdutosRela from "../../components/ProdutosRela";
-import TituloProducts from "../../components/TituloProduct";
+import ProductImage from "../../components/productpage/Product-image";
+import LocalPage from "../../components/productpage/ProductPageLocal";
+import ProdutosRela from "../../components/productpage/ProdutosRela";
+import TituloProducts from "../../components/productpage/TituloProduct";
 
 export default function ProductPage() {
     return(
         <div>
             <Header />
             <div className="flex bg-[#F9F8FE] ">  <LocalPage /></div> 
-          <section className="flex w-full h-[1000px]  bg-[#F9F8FE]">
+          <section className="flex w-full h-[750px]  bg-[#F9F8FE]">
          
             <ProductImage />
         <div className="relative top-[39px]">    <TituloProducts /> </div>
             </section> 
-            <section className="w-full h-[600px] bg-[#F9F8FE] ring-2">
+            <section className="w-full h-[600px] bg-[#F9F8FE]">
         <ProdutosRela />
         <div>
             
