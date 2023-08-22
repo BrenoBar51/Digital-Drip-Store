a7b48cbce6ccdd9b7b323ce1a77e5b71fc16eef7
BrenoBar51 <brenobarcelos9@gmail.com>
2023-08-21 22:53:23 -0300

    Finalizando page pre-compra

--- Digital-Store/src/routes/index.jsx
+++ Digital-Store/src/routes/index.jsx
@@ -1,55 +1,55 @@
-import { createBrowserRouter } from "react-router-dom";
-import ProductList from "../pages/Product-List/ProductList";
-import Home from "../pages/home/Home";
-import SignUpPage from "../pages/SignUp/SignUpPage";
-import LoginPage from "../pages/Login/LoginPage";
-import SignUpForm from "../pages/SignUp/SignUpForm";
-import ProductPage from "../pages/Product-Page/ProductPage";
-import ComprarPage from "../pages/comprar/ComprarPage";
-import Meuspedidos from "../pages/Meus-pedidosPG/Meuspedidos";
-import PreCompraPage from "../pages/PagePreCompra/PreCompra";
-import CompraRealizada from "../pages/CompraRealizada/CompraRealizada";
-
-
-export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />
-    },
-    {
-      path: "/productslist",
-      element: <ProductList />
-    },
-    {
-      path: "/login",
-      element: <LoginPage />
-    },
-    {
-      path: "/signup",
-      element: <SignUpPage />
-   },
-   {
-     path: "/signupform",
-     element: <SignUpForm />
-  },
-  {
-    path: "/productPage",
-    element: <ProductPage />
- },
-  {
-    path: "/comprar",
-    element: <ComprarPage />
-  },
-  {
-    path: "/meuspedidos",
-    element: <Meuspedidos />
-  },
-  {
-    path: "/precompra",
-    element: <PreCompraPage />
-  },
-  {
-    path: "/comprafinalizada",
-    element: <CompraRealizada />
-  }
-]);  
+import { createBrowserRouter } from "react-router-dom"; 
+import ProductList from "../pages/Product-List/ProductList"; 
+import Home from "../pages/home/Home"; 
+import SignUpPage from "../pages/SignUp/SignUpPage"; 
+import LoginPage from "../pages/Login/LoginPage"; 
+import SignUpForm from "../pages/SignUp/SignUpForm"; 
+import ProductPage from "../pages/Product-Page/ProductPage"; 
+import ComprarPage from "../pages/comprar/ComprarPage"; 
+import Meuspedidos from "../pages/Meus-pedidosPG/Meuspedidos"; 
+import PreCompraPage from "../pages/PagePreCompra/PreCompra"; 
+import CompraRealizada from "../pages/CompraRealizada/CompraRealizada"; 
+ 
+ 
+export const router = createBrowserRouter([ 
+    { 
+      path: "/", 
+      element: <Home /> 
+    }, 
+    { 
+      path: "/productslist", 
+      element: <ProductList /> 
+    }, 
+    { 
+      path: "/login", 
+      element: <LoginPage /> 
+    }, 
+    { 
+      path: "/signup", 
+      element: <SignUpPage /> 
+    }, 
+    { 
+     path: "/signupform", 
+     element: <SignUpForm /> 
+   }, 
+   { 
+    path: "/productPage", 
+    element: <ProductPage /> 
+  }, 
+  { 
+    path: "/comprar", 
+    element: <ComprarPage /> 
+  }, 
+  { 
+    path: "/meuspedidos", 
+    element: <Meuspedidos /> 
+  }, 
+  { 
+    path: "/precompra", 
+    element: <PreCompraPage /> 
+  }, 
+  { 
+    path: "/comprafinalizada", 
+    element: <CompraRealizada /> 
+  } 
+]);   
