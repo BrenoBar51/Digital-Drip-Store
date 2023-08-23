6a5ca0ca2e8f510e73bc33d16c7bd9e3106d6ef7
BrenoBar51 <brenobarcelos9@gmail.com>
2023-08-23 11:11:18 -0300

    ok

--- Digital-Store/src/Corpo/Footer.jsx
+++ Digital-Store/src/Corpo/Footer.jsx
@@ -4,20 +4,28 @@ import Contato from "../components/Contato";
 import Copyright from "../components/Copyright";
 import Informacoes from "../components/Informacoes";
 
-export default function Footer(){
-    return(
-        <div>
-            <footer className="bg-[#1F1F1F] flex flex-col">
-                <div className="grid ml-[4%] mt-[10%] mr-[4%] grid-cols-4" >
-                  <div className="col-span-1"> <AboutUs /> </div> 
-                  <div className="row-span-3">   <Informacoes /> </div>
-                  <div className="col-span-6s"> <Categorias /> </div>  
-                  <div className="col-span-2s"> <Contato /></div> 
-                </div>
-                <div>
-                  <Copyright />
-                </div>
-            </footer>
+export default function Footer() {
+  return (
+    <div>
+      <footer className="bg-[#1F1F1F] flex flex-col">
+        <div className="grid ml-[4%] mt-[10%] mr-[4%] grid-cols-4">
+          <div className="col-span-1">
+            <AboutUs />
+          </div>
+          <div className="row-span-3">
+            <Informacoes />
+          </div>
+          <div className="col-span-6s">
+            <Categorias />
+          </div>
+          <div className="col-span-2s">
+            <Contato />
+          </div>
         </div>
-    )
+        <div>
+          <Copyright />
+        </div>
+      </footer>
+    </div>
+  );
 }
