c1a6c41d9126f0dfdaad80c7c8311716ef59b9bb
berlimjr <gameruser815@gmail.com>
2023-08-28 10:55:41 -0300

    header 70%

--- Digital-Store/src/components/SearchBar.jsx
+++ Digital-Store/src/components/SearchBar.jsx
@@ -6,7 +6,7 @@ export default function SearchBar(){
             <div className="cursor-pointer hidden md:flex md:w-[32.5rem] lg:w-[32.5rem]">
                 <Input type={'text'} placeholder={'Pesquisar produtos...'} />
             </div>
-            <div className="cursor-pointer flex justify-start w-16 h-full rounded-r-md md:bg-gray-100 gap-x-10 items-center md:items-center">
+            <div className="cursor-pointer flex justify-start w-full h-full rounded-r-md md:bg-gray-100 gap-x-10 items-center md:items-center">
                 <img src="src\assets\image\Search.png" className="w-8 h-8 md:w-[18px] md:h-[19px]" alt="" />
             </div>
         </>
