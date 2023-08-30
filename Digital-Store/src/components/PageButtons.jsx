967b154f346b303135de85fb94bcdf072941bff1
berlimjr <gameruser815@gmai.com>
2023-08-30 10:15:05 -0300

    SideMenuMobile

--- Digital-Store/src/components/PageButtons.jsx
+++ Digital-Store/src/components/PageButtons.jsx
@@ -1,9 +1,13 @@
 import { NavLink } from "react-router-dom";
 
-export default function PageButtons() {
+export default function PageButtons(props) {
   return (
-    <div>
-      <ul className="flex gap-8">
+    <div className={`
+      ${props.className && props.className}
+    `}>
+      <ul className={`flex gap-8
+        ${props.className && props.className}
+      `}>
         <li className="cursor-pointer text-[#474747] font-bold hover:text-Rosa hover:underline">
           <NavLink to="/" className={({ isActive }) => (isActive ? "text-Rosa underline" : "")}>
             Home
