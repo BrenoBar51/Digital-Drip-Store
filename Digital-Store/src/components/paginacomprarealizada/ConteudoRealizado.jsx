bf3ba47b69c743ec3be20185a9108613f6aefc0c
SrOrfeu <orfeusancho@gmail.com>
2023-08-30 10:38:03 -0300

    comprarealizadarefeita

--- Digital-Store/src/components/paginacomprarealizada/ConteudoRealizado.jsx
+++ Digital-Store/src/components/paginacomprarealizada/ConteudoRealizado.jsx
@@ -4,83 +4,62 @@ export default function ConteudoCompraRealizada() {
   return (
     <div className="bg-Branco relative pl-[12%] mt-[5%] h-fit w-fit">
       <section>
-        <div className="text-[150px] relative flex justify-center">🎉</div>
+        <div className="text-[150px] md:text-[200px] relative flex justify-center md:flex-shrink-0 ">🎉</div>
         <div className="flex justify-center font-bold ">
           <div className="text-[46px] relative left-[130px] font-bold">Compra Realizada</div>
           <div className="text-[46px] relative top-[70px] right-[220px] font-bold">
             com sucesso!
           </div>
         </div>
-        <hr className="text-Cinza-Branco flex justify-center w-[1000px] relative left-[40px] top-[90px] font-extrabold" />
+        <hr className="text-Cinza-Branco  w-[1000px] w-[100%] ml-[5%] mt-[10%] font-extrabold" />
       </section>
-      <div className="relative bottom-[69px]">
-        <div className="text-Cinza-Escuro2 font-semibold relative top-[170px] left-[42px] text-[20px]">
+      <div className="ml-[5%]">
+        <h1 className="text-Cinza-Escuro2 font-semibold ml-[%] mt-[2%] ">
           Informações Pessoais
-        </div>
-        <div className="relative left-[42px] top-[200px]">
-          <div className="flex">Nome: Francisco Antonio Pereira</div>
-          <div className="relative left-[59px] bottom-[23px]"></div>
-        </div>
-        <div className="relative left-[42px] top-[210px]">
-          <div className="flex">CPF: 123485913-35</div>
-        </div>
-        <div className="relative left-[42px] top-[220px]">
-          <div className="flex">E-mail: francisco@gmail.com</div>
-        </div>
-        <div className="relative left-[42px] top-[230px]">
-          <div className="flex">Celular: (85) 5555-5555</div>
-        </div>
+        </h1>
+          <p className="flex">Nome: Orfeu Sancho Fernandes</p>
+          <p className="flex">CPF: 123485913-35</p>
+          <p className="flex">E-mail: Orfeu@gmail.com</p>
+          <p className="flex">Celular: (85) 5555-5555</p>
       </div>
-      <hr className="text-Cinza-Branco flex justify-center w-[1000px] relative left-[40px] top-[180px] font-extrabold" />
-
-      <div className="relative top-[20px]">
-        <div className="text-Cinza-light font-semibold relative top-[170px] left-[42px] text-[20px]">
-          Informações de entrega
-        </div>
-        <div className="relative left-[42px] top-[200px]">
-          <div className="flex">Endereço: Rua João Pessoa, 333</div>
-          <div className="relative left-[59px] bottom-[23px]"></div>
-        </div>
-        <div className="relative left-[42px] top-[210px]">
-          <div className="flex">Bairro: Centro</div>
-        </div>
-        <div className="relative left-[42px] top-[220px]">
-          <div className="flex">Cidade: Fortaleza, Ceará</div>
-        </div>
-        <div className="relative left-[42px] top-[230px]">
-          <div className="flex">Cep: 433-8800</div>
-        </div>
+      <hr className="text-Cinza-Branco flex w-[100%] ml-[5%] mt-[2%] font-extrabold" />
+    
+      <div className="ml-[5%]">
+        <h1 className="text-Cinza-Escuro2 font-semibold ml-[%] mt-[2%] ">
+          Informações de Entrega
+        </h1>
+          <p className="flex">Endereço: Rua Joao pessoa, 333</p>
+          <p className="flex">Bairro: Centro</p>
+          <p className="flex">Cidade: Fortaleza, Ceara</p>
+          <p className="flex">CEP: 433-8505</p>
       </div>
-      <hr className="text-Cinza-Branco flex justify-center w-[1000px] relative left-[40px] top-[270px] font-extrabold" />
-      <div className="text-Cinza-Escuro2 font-semibold relative top-[280px] left-[42px] text-[20px]">
-        Informações de Pagamento
+      <hr className="text-Cinza-Branco flex justify-center w-[1000px] mt-[2%] ml-[5%]  font-extrabold" />
+      <div className="ml-[5%]">
+        <h1 className="text-Cinza-Escuro2 font-semibold ml-[%] mt-[2%] ">
+          Informações de pagamento
+        </h1>
+          <p className="flex">Titular do Cartao: Orfeu S F</p>
+          <p className="flex">final: ***********2023 </p>
       </div>
-      <div className="relative top-[100px]">
-        <div className="relative left-[42px] top-[200px]">
-          <div className="flex">Titular do cartão: FRANCISCO A P</div>
-          <div className="relative left-[59px] bottom-[23px]"></div>
-        </div>
-        <div className="relative left-[42px] top-[210px]">
-          <div className="flex">Final ************2020</div>
-        </div>
-      </div>
-      <hr className="text-Cinza-Branco flex justify-center w-[1000px] relative left-[40px] top-[330px] font-extrabold" />
-      <div>
-        <div className="text-Cinza-Escuro2 font-semibold relative top-[340px] left-[42px] text-[20px]">
+      <hr className="text-Cinza-Branco flex justify-center w-[1000px] mt-[2%] ml-[5%]  font-extrabold" />
+    
+       
+        <h1 className="text-Cinza-Escuro2 font-semibold ml-[5%] mt-[2%]  ">
           Resumo da compra
-        </div>
-        <div className="w-[71.17px] h-[95px]">
+        </h1>
+        <div className="flex ml-[5%] mt-[4%] gap-4">
           <img
-            className={`w-[71.17px] h-[58px] shrink-0 rounded-[2.6px]  bg-Branco shadow-[0px_2.6691579818725586px_16.68223762512207px_0px_rgba(0,0,0,0.05)] relative top-[380px] left-[42px]`}
+            className="w-[71.17px] h-[58px] shrink-0 rounded-[2.6px] bg-Lilás "
             src="src\assets\image\CardTenisForIN.png"
             alt=""
           />
-        </div>
-        <div className="text-Cinza-Escuro text-[20px] relative top-[285px] left-[121px] font-bold ">
+        
+        <h1 className="text-Cinza-Escuro text-[20px] font-bold  ">
           Tênis Nike Revolution 6 Next <br /> Nature Masculino
+        </h1>
         </div>
-      </div>
-      <div className="bg-[#F6AA1C26] w-[1010px] h-[150px] relative left-[39px] top-[320px] ring-4 ring-[#f6aa1c4b] rounded ">
+       
+      <div className="bg-[#F6AA1C26] w-[1010px] h-[150px] mt-[6%] ml-[5%] ring-4 ring-[#f6aa1c4b] rounded ">
         <div className="text-[30px] relative top-[53px] left-[40px] font-bold ">Total</div>
         <div className="text-[30px] font-bold flex justify-end relative right-[20px] top-[8px] ">
           R$ 219,00
@@ -97,6 +76,7 @@ export default function ConteudoCompraRealizada() {
           Voltar para Home
         </button>
       </NavLink>
+       
     </div>
   );
 }
