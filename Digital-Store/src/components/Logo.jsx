c3977dee8ec4555057e0088e5083a3549a44dacc
BRENO BARCELOS DE OLIVEIRA <BRENO6130@DIGITALCOLLEGE.LOCAL>
2023-08-30 10:15:58 -0300

    fodase

--- Digital-Store/src/components/Logo.jsx
+++ Digital-Store/src/components/Logo.jsx
@@ -1,7 +1,7 @@
 export default function Logo(){
     return(
         <div className="flex gap-[3%] min-w-fit">
-            <img className="h-[2.13rem] pt-1 w-[2.13rem]" src="src\assets\image\LogoDigitalCollege.png" alt="Logo" />
+            <img className="h-[2.13rem] mt-[1.3%] w-[2.13rem]" src="src\assets\image\LogoDigitalCollege.png" alt="Logo" />
             <p className="text-[#C92071] text-4xl font-semibold min-w-fit">Digital Store</p>
         </div>
     )
