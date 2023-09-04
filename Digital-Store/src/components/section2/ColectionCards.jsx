b630fbce05a21acb3402b193676d058f216de8b5
BRENO BARCELOS DE OLIVEIRA <BRENO6130@DIGITALCOLLEGE.LOCAL>
2023-09-04 10:06:55 -0300

    ok

--- Digital-Store/src/components/section2/ColectionCards.jsx
+++ Digital-Store/src/components/section2/ColectionCards.jsx
@@ -1,6 +1,10 @@
 import FiveCardsIcons from "./FiveCardsIcons";
 import MicroTitle from "./MIcroTitle";
-import img from '../../assets/image/tshirt-_1_.png'
+import img1 from '../../assets/image/tshirt-_1_.png'
+import img2 from '../../assets/image/pants.png'
+import img3 from '../../assets/image/cap_icon-icons.com_59925.png'
+import img4 from '../../assets/image/headphones_1.png'
+import img5 from '../../assets/image/sneakers.png'
 
 export default function ColectionCards() {
     return (
@@ -10,11 +14,11 @@ export default function ColectionCards() {
                 <MicroTitle value3='Coleções em destaques' h2s2pt2='h2s2'/>
             </div>
             <div className="flex gap-x-12 md:flex-row ">
-                <FiveCardsIcons Fcards='Five' titulos='h1' valor3='Camisetas' icones={img} />
-                <FiveCardsIcons Fcards='Five' titulos='h1' valor3='Calças' icones='../../assets/image/pants.png' />
-                <FiveCardsIcons Fcards='Five' titulos='h1' valor3='Bonés' icones='../../assets/image/cap_icon-icons.com_59925.png' />
-                <FiveCardsIcons Fcards='Five' titulos='h1' valor3='HeadPhones' icones='../../assets/image/headphones_1.png' />
-                <FiveCardsIcons Fcards='Five' titulos='h1' valor3='Tênis' icones='../../assets/image/sneakers.png' />
+                <FiveCardsIcons Fcards='Five' titulos='h1' valor3='Camisetas' icones={img1} />
+                <FiveCardsIcons Fcards='Five' titulos='h1' valor3='Calças' icones={img2} />
+                <FiveCardsIcons Fcards='Five' titulos='h1' valor3='Bonés' icones={img3} />
+                <FiveCardsIcons Fcards='Five' titulos='h1' valor3='HeadPhones' icones={img4} />
+                <FiveCardsIcons Fcards='Five' titulos='h1' valor3='Tênis' icones={img5} />
             </div>
         </section>
         </>
