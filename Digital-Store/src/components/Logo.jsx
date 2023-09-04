2594ec66b09ea96ae15e78b629b14c5882a2d3b2
BRENO BARCELOS DE OLIVEIRA <BRENO6130@DIGITALCOLLEGE.LOCAL>
2023-09-04 09:09:27 -0300

    imagens

--- Digital-Store/src/components/Logo.jsx
+++ Digital-Store/src/components/Logo.jsx
@@ -1,9 +1,9 @@
-import img from '../assets/image/LogoDigitalCollege.png'
+// import img from '../assets/image/LogoDigitalCollege.png';
 
 export default function Logo(){
     return(
         <div className="flex gap-[3%] min-w-fit">
-            <img className="h-[2.13rem] mt-[1.3%] w-[2.13rem]" src={img} alt="Logo" />
+            <img className="h-[2.13rem] mt-[1.3%] w-[2.13rem]" src={'../assets/image/LogoDigitalCollege.png'} alt="Logo" />
             <p className="text-[#C92071] text-4xl font-semibold min-w-fit">Digital Store</p>
         </div>
     )
