4de0cfc403f34f525aeeb692c4914109c3eec802
BrenoBar51 <brenobarcelos9@gmail.com>
2023-11-20 23:41:18 -0300

    databases

--- Digital-Store/src/database.sql
+++ Digital-Store/src/database.sql
@@ -10,13 +10,13 @@ CREATE TABLE IF NOT EXISTS banners(
     banner_title_sup VARCHAR(50) NOT NULL,
     banner_discription VARCHAR(255) NOT NULL,
     banner_cta_text VARCHAR(20) NOT NULL,
-    banner_status INTEGER DEFAULT 1,
+    banner_status INTEGER DEFAULT 1
 );
 
 CREATE TABLE IF NOT EXISTS brands(
-    brand_id INTERGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    brand_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     brand_name VARCHAR(20) NOT NULL,
-    brand_status INTERGER DEFAULT 1 
+    brand_status INTEGER DEFAULT 1 
 );
 
 CREATE TABLE IF NOT EXISTS categories(
@@ -25,16 +25,16 @@ CREATE TABLE IF NOT EXISTS categories(
     category_status INTEGER DEFAULT 1
 );
 
+CREATE TABLE IF NOT EXISTS users(
+    user_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    user_name VARCHAR(20) NOT NULL,
+    user_status INTEGER DEFAULT 1
+);
+
 CREATE TABLE IF NOT EXISTS reviews(
     review_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     review_rate INTEGER NOT NULL,
     review_text VARCHAR(255) NULL,
     review_status INTEGER DEFAULT 1,
     FOREIGN KEY (user_id) REFERENCES users (user_id)
-);
-
-CREATE TABLE IF NOT EXISTS users(
-    users_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-    users_name VARCHAR(20) NOT NULL,
-    users_status INTEGER DEFAULT 1
 );
