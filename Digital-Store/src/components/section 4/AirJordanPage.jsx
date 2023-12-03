5edf7b6a5ae4ce48b594f41214ab269ca2e43aa0
BrenoBar51 <brenobarcelos9@gmail.com>
2023-12-02 22:30:40 -0300

    test image

--- Digital-Store/src/components/section 4/AirJordanPage.jsx
+++ Digital-Store/src/components/section 4/AirJordanPage.jsx
@@ -1,4 +1,5 @@
 import AirJordan from "./Airjordan";
+import img from '../assets/image/Laye1.png'
 import TitulosAirJordans from "./TituloAirJordans";
 
 export default function AirJordanPage() {
@@ -7,7 +8,7 @@ export default function AirJordanPage() {
       <section className="w-full flex md:flex-row flex-col gap-7 min-h-[600px]  p-24">
           <div className="relative flex items-center justify-center w-full h-full">
               <AirJordan />
-              <img className="bg-no-repeat absolute top-10 w-80 h-44 md:w-[573px] md:h-[330px] flex-shrink-0" src="src/assets/image/Laye1.png" alt="" />
+              <img className="bg-no-repeat absolute top-10 w-80 h-44 md:w-[573px] md:h-[330px] flex-shrink-0" src={img} alt="" />
           </div>
           <div className="flex flex-col gap-y-7 w-full h-full">
             <TitulosAirJordans />
